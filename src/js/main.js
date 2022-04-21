821932f02546e21a3b63a399cc5360362114f225
wudueuk <wudueuk@gmail.com>
2022-04-21 11:44:14 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+let count = 0;
+const imp = ['default', 'important', 'so-so']
+document.querySelector('.button-importance').addEventListener('click', ({target}) => {
+  count += 1;
+  if (count >= imp.length) {
+    count = 0
+  }
+
+  for (let i = 0; i < imp.length; i++) {
+    if (count === i) {
+      target.classList.add(imp[i])
+    } else {
+      target.classList.remove(imp[i])
+    }
+  }
+})
